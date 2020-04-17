0ac373ae6ffda42a0501670b3723c16c332619e1
t.j.c <tchockey92@gmail.com>
2020-04-17 16:28:06 -0400

    Added PageHeaderBase.cs

---
+++ src/Tayco.Web/Components/PageHeader/PageHeaderBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Tayco.Web.Components
+{
+    public class PageHeaderBase : ComponentBase
+    {
+        [Parameter]
+        public string Title { get; set; }
+    }
+}
