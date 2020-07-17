9afee8bae368d7b7b08fa4f2d0b22ffb3c2a5135
t.j.c <tchockey92@gmail.com>
2020-07-16 21:41:22 -0400

    Added Twitter contact link

--- src/Tayco.Web/Components/ContactLinks/ContactLinksBase.cs
+++ src/Tayco.Web/Components/ContactLinks/ContactLinksBase.cs
@@ -12,7 +12,8 @@ namespace Tayco.Web.Components
                 new Link("Gmail", "mailto:tayco2702@gmail.com", "envelope"),
                 new Link("LinkedIn", "https://www.linkedin.com/in/tayco2702/", "linkedin"),
                 new Link("Stack Overflow", "https://stackoverflow.com/users/5803406/devnull?tab=profile", "stack-overflow"),
-                new Link("Github", "https://github.com/t-j-c", "github")
+                new Link("Github", "https://github.com/t-j-c", "github"),
+                new Link("Twitter", "https://twitter.com/devNull_4", "twitter")
             };
 
             private Link(string name, string url, string icon)
