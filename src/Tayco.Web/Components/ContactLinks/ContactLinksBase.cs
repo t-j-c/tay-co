3ebbaad87a7534af98c806fe1f66dc83bf3ecb24
t.j.c <tchockey92@gmail.com>
2020-08-16 15:25:17 -0400

    Added loading spinner and copied font-awesome resources to wwwroot

--- src/Tayco.Web/Components/ContactLinks/ContactLinksBase.cs
+++ src/Tayco.Web/Components/ContactLinks/ContactLinksBase.cs
@@ -9,23 +9,28 @@ namespace Tayco.Web.Components
         {
             public static readonly IEnumerable<Link> All = new List<Link>
             {
-                new Link("Gmail", "mailto:tayco2702@gmail.com", "envelope"),
-                new Link("LinkedIn", "https://www.linkedin.com/in/tayco2702/", "linkedin"),
-                new Link("Stack Overflow", "https://stackoverflow.com/users/5803406/devnull?tab=profile", "stack-overflow"),
-                new Link("Github", "https://github.com/t-j-c", "github"),
-                new Link("Twitter", "https://twitter.com/devNull_4", "twitter")
+                SolidLink("Gmail", "mailto:tayco2702@gmail.com", "envelope"),
+                BrandLink("LinkedIn", "https://www.linkedin.com/in/tayco2702/", "linkedin"),
+                BrandLink("Stack Overflow", "https://stackoverflow.com/users/5803406/devnull?tab=profile", "stack-overflow"),
+                BrandLink("Github", "https://github.com/t-j-c", "github"),
+                BrandLink("Twitter", "https://twitter.com/devNull_4", "twitter")
             };
 
-            private Link(string name, string url, string icon)
+            private static Link SolidLink(string name, string url, string icon) => new Link(name, url, icon, "fas");
+            private static Link BrandLink(string name, string url, string icon) => new Link(name, url, icon, "fab");
+
+            private Link(string name, string url, string icon, string stylePrefix)
             {
                 Name = name;
                 Url = url;
                 Icon = icon;
+                StylePrefix = stylePrefix;
             }
 
             public string Name { get; }
             public string Url { get; }
             public string Icon { get; }
+            public string StylePrefix { get; }
         }
     }
 }
